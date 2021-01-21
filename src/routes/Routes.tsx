1d38e0123bb7a12a15ebdc3980950880416852c9
Teo Oliver <teo.liz.oli@gmail.com>
2021-01-21 15:54:11 +0000

    update: back to jest-expo/universal

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -47,7 +47,7 @@ export default function Routes() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#000000',
     // alignItems: "center",
     // justifyContent: "center",
   },
