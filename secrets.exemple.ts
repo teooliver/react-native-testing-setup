43830c6d18a98df7d3be3514a07fd9bc4916cf3e
Teo Oliver <teo.liz.oli@gmail.com>
2021-02-02 11:52:30 +0000

    secretes exemple

---
+++ secrets.exemple.ts
@@ -0,0 +1,3 @@
+export const OMDB_API_KEY = 'xxxxxxxx';
+export const API_URL = 'http://www.omdbapi.com/?apikey=xxxxxx&s=king';
+export const API_URL_GET_BY_ID = 'http://www.omdbapi.com/?apikey=xxxxx&i=';
